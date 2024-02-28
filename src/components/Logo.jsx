d37987ef06486ac2fa5f198e2d2b3384610d8a5a
MD EKRAM <Mekram47@gmail.com>
2024-02-28 23:19:26 +0530

    Blog Website Using React Redux

---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Logo(width = "100px") {
+  return (
+    <div>
+      <img
+        className="w-full h-20"
+        src="https://p7.hiclipart.com/preview/68/828/12/naver-blog-logo-computer-icons-naver-blog.jpg"
+        alt="Blog_Logo"
+      />
+    </div>
+  );
+}
+
+export default Logo;
